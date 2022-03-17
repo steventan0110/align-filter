b63f8a3d331910274164523989fa68cf184819fa
steven <wtan12@b08.clsp.jhu.edu>
2022-03-17 15:58:50 -0400

    update scripts & bug fix

--- scripts/qsub_pretrain.sh
+++ scripts/qsub_pretrain.sh
@@ -5,10 +5,12 @@ lang=ps
 resources='hostname=c*,mem_free=20G,ram_free=20G,gpu=1'
 for alignment_method in "wmt-align" ; do
   for filter_method in "laser" "sbert" "roberta"; do
+  #for filter_method in "laser"; do
     for subsample in 2 3 5 7; do
+    #for subsample in 2; do
       qsub -l $resources \
         -N pretrain-${alignment_method}-${filter_method} -j y \
-        -o $ROOT/qsub/pretrain-${alignment_method}-${filter_method}.out \
+        -o ${HOME}/qsub/pretrain-${alignment_method}-${filter_method}.out \
         ${SCRIPT}/pretrain.sh ${alignment_method} ${filter_method} ${subsample} ${lang}
     done
   done
