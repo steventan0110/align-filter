4f2d06ee18bb204c0a08a69b0a128ac6ab145181
Steven Tan <wtan12@jhu.edu>
2022-03-18 12:24:46 -0400

    update qsub script to check if LASER works in this version

---
+++ scripts/exp/qsub_general.sh
@@ -0,0 +1,15 @@
+lang=$1
+alignment_method=$2
+filter_method=$3
+if [[ ${HOME} == '/home/wtan12' ]]; then
+  resources='hostname=c*,mem_free=20G,ram_free=40G,gpu=1,cpu=10'
+  job="${lang}-${alignment_method}-align-${filter_method}-filter"
+  qsub \
+  	  -l $resources \
+  	  -N $job -j y \
+  	  -o $HOME/qsub/$job.out \
+  	  ${HOME}/align-filter/scripts/main.sh ${alignment_method} ${filter_method} $lang
+else
+  # execute home environment
+  bash ${HOME}/Code/GITHUB/align-filter/scripts/main.sh ${alignment_method} ${filter_method} $lang
+fi
