ab3c7734071ee9d5ae0de54e4b47a86532a2290b
steven <wtan12@b08.clsp.jhu.edu>
2022-03-28 20:07:45 -0400

    force roberta to use laser trained version

--- scripts/config/roberta_filter_config.sh
+++ scripts/config/roberta_filter_config.sh
@@ -1,4 +1,6 @@
 export filter_method="roberta"
-export PROXY_TASK_NAME=${alignment_type}
+#export PROXY_TASK_NAME=${alignment_type}
+# hard code the roberta model to be laser-align
+export PROXY_TASK_NAME="laser-align"
 export roberta_max_seq_length=256
 export roberta_mode=classification
